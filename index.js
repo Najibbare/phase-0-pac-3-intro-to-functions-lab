9aebdc0a23fbfb44a83c2a89241eb9b856c90a61
+ <+>
2022-08-04 03:01:10 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+	return string.toUpperCase();
+}
+
+function whisper(string) {
+	return string.toLowerCase();
+}
+
+function logShout(string) {
+	console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+	console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+	if (string === string.toLowerCase());
+	return ("I can't hear you!");
+
+}
+
+function sayHiToHeadphonedRoommate(string){
+    let CantHearYou = "I can't hear you!";
+    let CanHearYou = "YES INDEED!";
+    let LoveAnswer = "I would love to!";
+    if(string.toLowerCase(string)===string){
+        return CantHearYou;
+    }
+    else if (string.toUpperCase(string)===string){
+        return CanHearYou;
+    }
+    else if("Let's have dinner together!"===string){
+        return LoveAnswer;
+    }
+
+}
